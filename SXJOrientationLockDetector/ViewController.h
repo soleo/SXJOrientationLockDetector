3da0f14c9a4018bd032377d206442b1acf0dbedc
Xinjiang Shao <shaoxinjiang@gmail.com>
2015-11-30 21:35:30 -0600

    Initial Commit

---
+++ SXJOrientationLockDetector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SXJOrientationLockDetector
+//
+//  Created by Xinjiang Shao on 11/30/15.
+//  Copyright © 2015 Xinjiang Shao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
